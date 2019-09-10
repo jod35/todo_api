214c7f1eda0cb3de54b2c3711d60311d621033d0
jod35 <jodestrevin@gmail.com>
2019-09-10 09:35:27 -0400

    completed roles of the create_user and get_all_users

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import uuid
+from werkzeug.security import generate_password_hash,check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 #CONFIG
@@ -27,7 +29,24 @@ class Todo(db.Model):
 #routes
 @app.route('/user',methods=['GET'])
 def get_all_users():
-   return ''
+   #this query returns all the users from the database
+
+   users=User.query.all()
+
+   output=[]
+   #since sqlachemy results cannot be returned directly as json, we make our own json objects from the results.
+
+   for user in users:
+       user_data={}
+
+       user_data['public_id']=user.public_id
+       user_data['name']=user.name
+       user_data['password']=user.password
+       user_data['admin']=user.admin
+       #we then add the dictionaries to our output lists
+       output.append(user_data)
+
+   return jsonify({"users":output})
     
 @app.route('/user/<user_id>',methods=['GET'])
 def get_one_user():
@@ -35,7 +54,12 @@ def get_one_user():
 
 @app.route('/user', methods=['POST'])
 def create_user():
-    return ''
+    data= request.get_json()
+    hashed_password=generate_password_hash(data['password'],method='sha256')
+    new_user=User(public_id=str(uuid.uuid4()),name=data['name'],password=hashed_password,admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message":"New User Added Successfully"})
 
 @app.route('/user/<user_id>', methods=['PUT'])
 def promote_user():
